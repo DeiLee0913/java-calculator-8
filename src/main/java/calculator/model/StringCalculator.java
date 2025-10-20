ddf74636d915132e4e175ccc8a5a6daa622bcfe1
D <deilee0913@gmail.com>
2025-10-20 16:40:30 +0900

    feat(model): 문자열 파싱 기능 구현
    - 커스텀 지정자 추출
    - 입력문에서 숫자 추출

--- src/main/java/calculator/model/StringCalculator.java
+++ src/main/java/calculator/model/StringCalculator.java
@@ -5,9 +5,27 @@ public class StringCalculator {
         return input == null || input.isEmpty();
     }
 
+    public String[] parseInput(String input) {
+        String dividers = ":,";
+
+        // Extract Custom Splitters
+        if (input.startsWith("//")) {
+            int idx =  input.indexOf("\\n");
+            dividers += input.substring(2, idx);
+            input = input.substring(idx + 2);
+        }
+
+        // Extract numbers by splitting
+        return input.split("[" + dividers + "]");
+    }
+
+
+
     public int calculate(String input) {
         if (isStringEmpty(input)) {
             return 0;
+        } else {
+//            return (parseInput(input));
         }
     }
 }
