b2bbcce6fb84b4c6574bfff856349cdd8d0f6999
D <deilee0913@gmail.com>
2025-10-20 16:45:15 +0900

    feat(app): 프로그램 진입점 및 컴포넌트 초기화 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.controller.CalculateController;
+import calculator.model.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringCalculator stringCalculator = new StringCalculator();
+        CalculateController calculateController = new CalculateController(inputView, outputView, stringCalculator);
+
+        calculateController.run();
     }
 }
